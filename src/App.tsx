702cd70d70d3bab437ef03b3d7101726618c8ecd
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-05-04 10:54:07 -0300

    installing icons libary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import { Widget } from './components/Widget';
 
 export const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>This is a boilerplate for a react app.</p>
-      <button className="bg-amber-800">MOJO</button>
+      <Widget />
     </div>
   );
 };
---
+++ src/components/Widget.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ChatTeardropDots } from 'phosphor-react'
+
+export const Widget = () => {
+  return (
+    <div>
+      <ChatTeardropDots />
+    </div>
+  )
+}
